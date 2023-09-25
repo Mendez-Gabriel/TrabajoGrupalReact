3a19b8362e71084260d8f870f1b3d11f9652e21b
bautistaarias <bautiariaaas@gmail.com>
2023-09-25 18:14:45 -0300

    diseños añadadidos al archivo CardProduct

--- src/components/specific/Card/CardProduct.jsx
+++ src/components/specific/Card/CardProduct.jsx
@@ -1,33 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import styles from './CardProduct.modules.css';
+// import axios from 'axios';
 
-function CardProduct() {
+
+function CardProduct({image, title, description, price}) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get("https://fakestoreapi.com/products/category/electronic")
-    .then(({data}) => {
-      console.log(data)
-    } )
-    .catch(() => {alert('no se encontro la pag')})
-    .finally(() => {})
-    // fetch('https://fakestoreapi.com/products')
-    //   .then((response) => response.json())
-    //   .then((data) => setProducts(data));
+    // axios.get("https://fakestoreapi.com/products/category/electronic")
+    // .then(({data}) => {
+    //   console.log(data)
+    // } )
+    // .catch(() => {alert('no se encontro la pag')})
+    // .finally(() => {})
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
   }, []);
 
   return (
-    <div className="card-producto">
+    <div className="card-prodct">
       {products.map((product) => (
-        <div key={product.id} className="card">
-          <img src={product.image} alt={product.title} className='imagenCard' />
+        <div key={product.id} className="card-product">
+          <img src={product.image} alt={product.title}></img>
           <h1>{product.title}</h1>
-          <p className='card-text'>Price: ${product.price}</p>
-        </div>
+          <p>{product.description}</p>
+          <div className='precio mb-3'>${product.price}</div>
+            <span className={`leer-mas ${product.description.length > 1 ? 'active' : '' }`}><a href='./Item/Item.jsx'>Leer más</a></span>
+          </div>
       ))}
     </div>
   );
 }
 
 export default CardProduct;
-
