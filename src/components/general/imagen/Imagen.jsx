95086d3ca3a0a6b33b44c5c770b6ba9c70d56e80
Gabriel Mendez <mendez.gabriel.d@hotmail.com>
2023-09-25 17:28:27 -0300

    Vinculacion de productos con Routes  y correcciones a carrousel

--- src/components/general/imagen/Imagen.jsx
+++ src/components/general/imagen/Imagen.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import Homepages from '../../../pages/Homepages';
+import { Link } from 'react-router-dom';
 import style from './Imagen.module.css'
 
-const Imagen = ({ img, Title }) => {
+const Imagen = ({ img, Title, id }) => {
 
-    const { imgProduct } = style;
+    const { imgProduct, btnProduct } = style;
 
     return (
 
         <div className=' d-flex flex-column '>
-            <img src={img} className={`d-block ${imgProduct} mx-4`} alt="..." />
-            <h5 className='text-center mx-4'>{Title}</h5>
+            <Link to={`/productos/${id}`}>
+                <button className={`btn btn-light rounded-4 mx-4 ${btnProduct}`}>
+                    <img src={img} className={`d-block ${imgProduct} mx-4`} alt="..." />
+                    <h5 className='text-center mx-5 m-3'>{Title}</h5>
+                </button>
+            </Link>
         </div>
 
     )
