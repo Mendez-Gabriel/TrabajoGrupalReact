c41c24b6f0c8ee7d5e92ddf505d95ec8d8082873
Emiliano Augusto Gasco <emilianogasco99@gmail.com>
2023-09-26 16:56:29 -0300

    actualice item.jsx

--- src/components/specific/Item/Item.jsx
+++ src/components/specific/Item/Item.jsx
@@ -3,12 +3,17 @@ import axios from 'axios'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import ItemStart from '../ItemStart/ItemStart'
 import { useParams } from 'react-router-dom'
+import Carrousels from '../../general/Carrousel/Carrousels'
 
 const Item = (
+
   // los props estan silenciados asi no haya error
   // {image, title, description, category, price}
-  ) => {
-    let params = useParams()
+) => {
+  const urlBase = `https://fakestoreapi.com/products/`;
+  const [info, setInfo] = useState([]);
+  let params = useParams()
+
   const [item, setItem] = useState({})
   useEffect(() => {
     axios.get(`https://fakestoreapi.com/products/${params.id}`)
@@ -20,14 +25,21 @@ const Item = (
       .finally(() => { })
 
   }, [])
+  
+  useEffect(() => {
+    const { data } = axios.get(urlBase)
+      .then(({ data }) => { setInfo(data); })
+      .catch((error) => console.log(error))
+  }, []);
+
   //  desconstruyo lo que es el item, para despues muetearlo y desmutear los props
-  const { image, title, description, category,price } = item
+  const { image, title, description, category, price } = item
 
   return (
-    <div className="container">
+    <div className="container ">
 
-      <div className="card mb-3 " >
-        <div className="row g-0 text-start">
+      <div className="card my-5" >
+        <div className="row g-0">
           <div className="col-md-4 text-center">
             <img src={image} className="img-fluid rounded-start w-75" alt="..." />
           </div>
@@ -46,14 +58,18 @@ const Item = (
                 </div>
               </div>
               <h5 className="card-title">${price}</h5>
-              <div className="input-group mb-3 w-50">
-                <input type="number" min={1} className="form-control" aria-describedby="button-add" />
-                <button className="btn btn-outline-success" type="button" id="button-add">Agregar al Carrito <i className="bi bi-cart-check"></i></button>
+              <div className="input-group mb-3 w-50 mx-auto">
+                <input type="number" min={1} className="form-control form-control-sm" aria-describedby="button-add" />
+                <button className="btn btn-sm btn-outline-success" type="button" id="button-add">Agregar al Carrito <i className="bi bi-cart-check"></i></button>
               </div>
 
             </div>
           </div>
         </div>
+      </div>
+      <div className='text-info mb-3'>
+      <Carrousels infoImage={info}/>
+
       </div>
     </div>
 
