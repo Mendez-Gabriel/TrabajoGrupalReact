7f9ca7a656d9da2d68b70ca90f54fdfd04abc89c
Emiliano Augusto Gasco <emilianogasco99@gmail.com>
2023-09-26 18:31:10 -0300

    acutalize productos

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import CardProducto from '../components/specific/Card/CardProduct'
+import CardProducto from '../components/specific/CardProduct/CardProduct'
 import axios from 'axios'
 
 const Productos = () => {
